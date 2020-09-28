41c92f6167665b88069c786e51ced4cec4bad389
Hanyuan Dong <stedong@ucdavis.edu>
2020-09-27 18:09:08 -0700

    Revert "Merge latest"

--- backend/src/main/java/com/bulletjournal/templates/repository/UserCategoryDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/templates/repository/UserCategoryDaoJpa.java
@@ -2,14 +2,10 @@ package com.bulletjournal.templates.repository;
 
 import com.bulletjournal.exceptions.ResourceNotFoundException;
 import com.bulletjournal.repository.ProjectRepository;
-import com.bulletjournal.repository.UserDaoJpa;
 import com.bulletjournal.repository.models.Project;
 import com.bulletjournal.repository.models.User;
-import com.bulletjournal.templates.repository.model.SelectionMetadataKeyword;
 import com.bulletjournal.templates.repository.model.UserCategory;
 import com.bulletjournal.templates.repository.model.UserCategoryKey;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
@@ -21,8 +17,6 @@ import java.util.stream.Collectors;
 
 @Repository
 public class UserCategoryDaoJpa {
-    private static Logger LOGGER = LoggerFactory.getLogger(UserCategoryDaoJpa.class);
-
     @Autowired
     private UserCategoryRepository userCategoryRepository;
 
@@ -33,22 +27,13 @@ public class UserCategoryDaoJpa {
     private ProjectRepository projectRepository;
 
     @Autowired
-    private SelectionMetadataKeywordDaoJpa selectionMetadataKeywordDaoJpa;
-
-    @Autowired
-    private UserDaoJpa userDaoJpa;
+    private SelectionMetadataKeywordRepository selectionMetadataKeywordRepository;
 
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
     public void save(UserCategory userCategory) {
         if (userCategory != null) {
             userCategoryRepository.save(userCategory);
         }
-    }
-
-    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
-    public List<UserCategory> getUserCategoriesByUserName(String username) {
-        User user = userDaoJpa.getByName(username);
-        return userCategoryRepository.getAllByUser(user);
     }
 
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
@@ -68,30 +53,24 @@ public class UserCategoryDaoJpa {
     public void updateUserCategory(User user, Long categoryId, List<Long> selections, Long projectId) {
         Project project = this.projectRepository.findById(projectId).orElseThrow(() -> new ResourceNotFoundException(
                 "Project " + projectId + " not found"));
-        List<SelectionMetadataKeyword> keywords = this.selectionMetadataKeywordDaoJpa
-                .getKeywordsBySelections(selections);
-        for (SelectionMetadataKeyword keyword : keywords) {
-            saveUserSubscription(user, categoryId, selections, project, keyword);
-        }
-    }
-
-    private void saveUserSubscription(
-            User user, Long categoryId, List<Long> selections, Project project, SelectionMetadataKeyword keyword) {
         UserCategoryKey userCategoryKey = new UserCategoryKey(
-                user.getId(), categoryId, keyword.getKeyword());
+                user.getId(), categoryId, "LEETCODE_ALGORITHM");
         UserCategory userCategory;
         if (!checkExist(userCategoryKey)) {
             userCategory = new UserCategory();
-            userCategory.setSelections(selections.stream().distinct().map(Object::toString).collect(Collectors.joining(",")));
+            if (selections != null) {
+                userCategory.setSelections(selections.stream().distinct().map(Object::toString).collect(Collectors.joining(",")));
+            }
             userCategory.setCategory(this.categoryDaoJpa.getById(categoryId));
             userCategory.setUser(user);
             userCategory.setUserCategoryKey(userCategoryKey);
             userCategory.setProject(project);
-            userCategory.setMetadataKeyword(keyword);
+            userCategory.setMetadataKeyword(
+                    selectionMetadataKeywordRepository.findById("LEETCODE_ALGORITHM")
+                            .orElseThrow(() -> new ResourceNotFoundException("LEETCODE_ALGORITHM not found")));
         } else {
             userCategory = getUserCategoryByKey(userCategoryKey);
-            Set<String> selectionSet = userCategory.getSelectionIds()
-                    .stream().map(Object::toString).collect(Collectors.toSet());
+            Set<String> selectionSet = userCategory.getSelectionIds().stream().map(Object::toString).collect(Collectors.toSet());
             if (selections != null) {
                 selectionSet.addAll(selections.stream().map(Object::toString).collect(Collectors.toList()));
             }
